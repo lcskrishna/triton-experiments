9cc613e4d6c9c76fadd1242314bbeb1fd319391d
lcskrishna <lollachaitanya@gmail.com>
2025-05-02 06:48:37 +0000

    add benchmarking for vector add

---
+++ vector_add/vector_add_0502.py
@@ -0,0 +1,77 @@
+import torch
+
+import triton
+import triton.language as tl
+
+DEVICE = 'cuda'
+
+@triton.jit
+def add_kernel(x_ptr,
+               y_ptr,
+               output_ptr,
+               n_elements,
+               BLOCK_SIZE: tl.constexpr,
+               ):
+
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    #grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']))
+
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    return output
+
+
+## quick_test
+torch.manual_seed(0)
+size = 98432
+x = torch.rand(size, device='cuda')
+y = torch.rand(size, device='cuda')
+output_torch = x + y
+output_triton = add(x, y)
+print (output_torch)
+print ("----------------")
+print (output_triton)
+
+print(f'The maximum difference between torch and triton is '
+      f'{torch.max(torch.abs(output_torch - output_triton))}')
+
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names= ['size'],
+        x_vals = [2**i for i in range(12, 28, 1)],
+        x_log = True,
+        line_arg = 'provider',
+        line_vals = ['triton', 'torch'],
+        line_names = ['Triton', 'Torch'],
+        styles=[('blue', '-'), ('green', '-')],
+        ylabel='GB/s',
+        plot_name='Vector-add-performance',
+        args={},
+))
+def benchmark(size, provider):
+    x = torch.randn(size, device='cuda', dtype=torch.float32)
+    y = torch.randn(size, device='cuda', dtype=torch.float32)
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+
+    gbps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms*1e-3)
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+
+benchmark.run(print_data=True, save_path='results')
